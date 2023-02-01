0ad674172d76eb8d86f7762e4a18650c68e2296e
Yash Diwaker <190104076@hbtu.ac.in>
2023-02-01 17:56:17 +0530

    again update

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -2,21 +2,23 @@ import React from "react";
 import "./Header.css";
 import Json_data from "../Constants/Json_Data.json";
 import { MdKeyboardArrowDown } from "react-icons/md";
+import { FaSearch } from "react-icons/fa";
 import Sliderr from "./Sliderr";
 
 const Header = () => {
+  //Here we set jsondata to data
   const [data, setdata] = React.useState(Json_data.categoryItem);
-  // console.log(data[0].Category);
+  const [input, setInput] = React.useState('');
+  console.log(input);
   return (
     <>
       <div className="Header">
         {data.map((key, index) => {
-          {
-            console.log(key.Category[0].subCategory);
-          }
           return (
             <div className="category">
-              <div className="categoty_image">
+              <div className="categoty_image"
+              //  onMouseEnter={(e)=>{e.target.style.background='#000000'}} onMouseLeave={(e)=>{e.target.style.background='#fff'}}
+              >
                 <img src={key?.image} alt="/" />
               </div>
               <div className="category_name">
@@ -30,6 +32,33 @@ const Header = () => {
                   </span>
                 )}
               </div>
+              {key.Category[0].subCategory.length > 0 && <div className="dropdown-container">
+              <div className="Search_bar" style={{width:'90%',margin:'auto',border:'0.5px solid rgba(110, 110, 110, 0.067)',height:'40px'}}>
+                <input type="text" placeholder="Search" onChange={(e)=>setInput(e.target.value)}/>
+                <FaSearch className={"SearchIcon"} style={{fontSize:'1rem'}}/>
+              </div>
+                {key.Category.filter((v)=>{
+                  const value = v.Category_name.toLowerCase();
+                  const inValue = input.toLowerCase();
+                  return(
+                    value.includes(inValue)
+                    )
+                }).map((itm)=>(
+                  <div className="dropdown">
+                    <div className="dropdown-item">
+                      <div>{itm.Category_name}</div>
+                      <div className="dropdown-item-icon">
+                        <MdKeyboardArrowDown style={{transform:'rotate(-90deg)'}}/>
+                      </div>
+                    </div>
+                    {itm.subCategory.length!==0 && <div className="subelement" position='absolute' left='100%'>{itm.subCategory.map((it)=>(
+                      <div className="subelement-item">
+                        <div className="subelement-item-element">{it}</div>
+                      </div>
+                    ))}</div>}
+                  </div>
+                ))}
+              </div>}
             </div>
           );
         })}
--- src/Component/NavBar.jsx
+++ src/Component/NavBar.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
 import "./NavBar.css"
+import {FaSearch,FaShoppingCart} from "react-icons/fa"
 
 const Header = () => {
   return (
     <>
     <div className='navbar'>
            <div className="logo">
-            
+            Ecommerce
            </div>
+      {/* <button className="LBtn">Login</button> */}
+      <div className='links'>
+        <div className={"SellerInfo"}>Home</div>
+        <div className={"SellerInfo"}>About</div>
+        <h3 className={"MoreInfo"}>Shop</h3>
+        <div className={"SellerInfo"}>Contact</div>
+        <div className={"CartInfo"}> <span className="Cart"><FaShoppingCart></FaShoppingCart></span >Cart</div>
+      </div>
+      <div className="Search_bar">
+        <input type="text" placeholder="Search" />
+        <FaSearch className={"SearchIcon"} />
+      </div>
     </div>
     </>
   )
